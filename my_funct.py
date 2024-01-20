7c8197666d04a4664dedacfc1ef65eebb743d3bd
Srjohnson589 <srjohnson4586@gmail.com>
2024-01-20 17:43:50 -0500

    half of second problem done

---
+++ my_funct.py
@@ -0,0 +1,10 @@
+#these are my functions to calculate square footage of a house
+#and the circumference of a circle
+
+import math
+
+def area(l, w):
+    return l*w
+
+def circum(r):
+    return 2*pi()*r
